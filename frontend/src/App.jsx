41db173985c47b165e717c162e4068c58175cad9
Kausuru <kaushika_mki_e23@engug.ruh.ac.lk>
2025-01-04 11:07:12 +0530

    Did Until Searchbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,13 @@ import Home from './pages/Home'
 import Collection from './pages/Collection'
 import Contact from './pages/Contact'
 import Footer from './components/Footer'
+import SearchBar from './components/SearchBar'
 
 const App = () => {
   return (
     < div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <Navbar />
+      <SearchBar/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/collection' element={<Collection/>} />
